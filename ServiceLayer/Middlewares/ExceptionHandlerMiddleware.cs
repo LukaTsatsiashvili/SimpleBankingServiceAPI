650f78cdd12feec45d43f1a963c33a80fa3fdb89
luka tsatsiashvili <luka.tsatsiashvili.1@btu.edu.ge>
2024-08-07 22:14:33 +0400

    Improved Global Exception Handler Middleware.

--- ServiceLayer/Middlewares/ExceptionHandlerMiddleware.cs
+++ ServiceLayer/Middlewares/ExceptionHandlerMiddleware.cs
@@ -1,7 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Net;
-using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Text.Json;
 
 namespace ServiceLayer.Middlewares
 {
@@ -9,28 +10,65 @@ namespace ServiceLayer.Middlewares
 		ILogger<ExceptionHandlerMiddleware> logger,
 		RequestDelegate next)
 	{
+
 		public async Task InvokeAsync(HttpContext context)
 		{
+			// Declare variables
+			string title = "Warrning";
+			string message = "Internal Server Error";
+			int statusCode = (int)HttpStatusCode.InternalServerError;
+
 			try
 			{
 				await next(context);
+
+				// If response is Unauthorized - 401 
+				if (context.Response.StatusCode == StatusCodes.Status401Unauthorized)
+				{
+					title = "Unauthorized";
+					message = "Unauthorized request! Please authenticate.";
+					statusCode = (int)HttpStatusCode.Unauthorized;
+					
+					await ModifyHeader(context, title, message, statusCode);
+				}
+
+				// If response is Forbidden - 403
+				if (context.Response.StatusCode == StatusCodes.Status403Forbidden)
+				{
+					title = "Forbidden";
+					message = "Forbidden request!";
+					statusCode = (int)HttpStatusCode.Forbidden;
+
+					await ModifyHeader(context, title, message, statusCode);
+				}
 			}
 			catch (Exception ex)
 			{
 				var exceptionId = Guid.NewGuid();
 				logger.LogError(ex, "{ErrorId} : {ErrorMessage}", exceptionId, ex.Message);
 
-				context.Response.ContentType = "application/json";
-				context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
-				var exception = new
+				if (ex is TaskCanceledException || ex is TimeoutException)
 				{
-					Id = exceptionId,
-					Message = "An error occurred while processing the request.",
-				};
+					title = "Out of time!";
+					message = "Request timeout ... Try again later.";
+					statusCode = (int)HttpStatusCode.RequestTimeout;
+				}
 
-				await context.Response.WriteAsJsonAsync(exception);
+				await ModifyHeader(context, title, message, statusCode);
 			}
+		}
+
+		private static async Task ModifyHeader(HttpContext context, string title, string message, int statusCode)
+		{
+			context.Response.ContentType = "application/json";
+
+			await context.Response.WriteAsync(JsonSerializer.Serialize(new ProblemDetails
+			{
+				Detail = message,
+				Status = statusCode,
+				Title = title
+			}),CancellationToken.None);
 		}
 	}
 }
