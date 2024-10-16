c6c89438bd73c45ea0169e7d1c0c0dd06a5eb5c9
luka tsatsiashvili <luka.tsatsiashvili.1@btu.edu.ge>
2024-10-16 17:02:00 +0400

    Created Tests for Transaction and Auth Controllers.

---
+++ Test/Controller/AuthControllerTest.cs
@@ -0,0 +1,118 @@
+﻿using EntityLayer.DTOs.Auth;
+using ServiceLayer.Services.Auth.Abstract;
+
+namespace Test.Controller;
+
+public class AuthControllerTest : BaseControllerTest<AuthController>
+{
+	private readonly IAuthService _service;
+
+    public AuthControllerTest()
+    {
+        // Set up dependencies
+        _service = A.Fake<IAuthService>();
+
+        // SUT -> System Under Test
+        InitializeController(CreateController());
+        SetUpControllerContext();
+    }
+
+    protected override AuthController CreateController()
+	{
+        return new AuthController(_service);
+	}
+
+
+    // Create
+    // This method returns Ok(success) | BadRequset(fails) action results
+    [Fact]
+    public async void AuthController_Register_ReturnsOk()
+    {
+        // Arrange
+        UserDTO modelDTO = new()
+        {
+            Email = "Test@email.com",
+            Password = "TestPassword123@",
+            ConfirmPassword = "TestPassword123@"
+		};
+        GeneralResponse mockResponse = new(true, null!);
+
+        // Act
+        A.CallTo(() => _service.RegisterUserAsync(modelDTO)).Returns(mockResponse);
+        var result = (OkObjectResult) await _controller.Register(modelDTO);
+
+		// Assert
+		result.StatusCode.Should().Be(200);
+		result.Should().NotBeNull();
+		result.Value.Should().BeEquivalentTo(mockResponse.Message);
+	}
+
+    // Create 
+    // This method returns Ok(success) | BadRequest(fails) action results
+    [Fact]
+    public async void AuthController_Login_ReturnsOk()
+    {
+        // Arrange
+        LoginDTO modelDTO = new()
+        {
+            Email = "Test@email.com",
+            Password = "TestPassword123@"
+        };
+        LoginResponse mockResponse = new(true, "Test_JWTBearer_Token", null!);
+
+        // Act
+        A.CallTo(() => _service.LoginAsync(modelDTO)).Returns(mockResponse);
+        var result = (OkObjectResult) await (_controller.Login(modelDTO));
+
+		// Assert
+		result.StatusCode.Should().Be(200);
+		result.Should().NotBeNull();
+		result.Value.Should().BeEquivalentTo(mockResponse);
+	}
+
+    // Create 
+    // This method returns Ok(success) | BadRequest(fails) action results
+    [Fact]
+    public async void AuthController_ForgotPassword_ReturnsOk()
+    {
+        // Arrange
+        ForgotPasswordDTO modelDTO = new()
+        {
+            Email = "Test@email.com"
+        };
+        ForgotPasswordResponse mockResponse = new(true, null!);
+
+        // Act
+        A.CallTo(() => _service.ForgotPasswordAsync(modelDTO)).Returns(mockResponse);
+        var result = (OkObjectResult)await _controller.ForgotPassword(modelDTO);
+
+		// Assert
+		result.StatusCode.Should().Be(200);
+		result.Should().NotBeNull();
+		result.Value.Should().BeEquivalentTo(mockResponse.Message);
+	}
+
+    // Create 
+    // This method returns Ok(success) | BadRequest(fails) action results
+    [Fact]
+    public async void AuthController_ResetPassword_ReturnsOk()
+    {
+        // Arrange
+        ResetPasswordDTO modelDTO = new()
+        {
+            UserId = userId,
+            NewPassword = "NewTestPassword123@",
+            Token = "Test_Token"
+        };
+        ResetPasswordResponse mockResponse = new(true, null!);
+
+        // Act
+        A.CallTo(() => _service.ResetPasswordAsync(modelDTO)).Returns(mockResponse);
+        var result = (OkObjectResult)await _controller.ResetPassword(modelDTO);
+
+		// Assert
+		result.StatusCode.Should().Be(200);
+		result.Should().NotBeNull();
+		result.Value.Should().BeEquivalentTo(mockResponse.Message);
+	}
+}
---
+++ Test/Controller/TransactionControllerTest.cs
@@ -0,0 +1,44 @@
+﻿using EntityLayer.DTOs.Transaction;
+
+namespace Test.Controller;
+
+public class TransactionControllerTest : BaseControllerTest<TransactionController>
+{
+	private readonly ITransactionService _service;
+
+    public TransactionControllerTest()
+    {
+        // Set up dependencies
+        _service = A.Fake<ITransactionService>();
+
+        // SUT -> System Under Test
+        InitializeController(CreateController());
+        SetUpControllerContext();
+    }
+
+    protected override TransactionController CreateController()
+	{
+		return new TransactionController(_service);
+	}
+
+
+    // Create
+    // This method returns Ok(success) | BadRequest(fails) action results
+    [Fact]
+    public async void TransactionController_Create_ReturnsOk()
+    {
+        // Arrange
+        TransactionDTO modelDTO = new(150, "11/20/2021", "1234567890", "1345678920");
+        TransactionCreateDTO transactionDTO = new(150, "1345678920");
+        TransactionResponse mockResponse = new (true, null!, "1234567890", modelDTO);
+
+        // Act
+        A.CallTo(() => _service.CreateTransactionAsync(userId, transactionDTO)).Returns(mockResponse);
+        var result = (OkObjectResult)await _controller.CreateTransaction(transactionDTO);
+
+		// Assert
+		result.StatusCode.Should().Be(200);
+		result.Should().NotBeNull();
+		result.Value.Should().BeEquivalentTo(mockResponse);
+	}
+}
